f466a73783862f26b6adb5b0e9143da4d6e28ca1
backas36 <backas36@gmail.com>
2023-12-27 23:46:41 +0800

    chore: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import mdx from "@astrojs/mdx";
 import netlify from "@astrojs/netlify";
 import tailwind from "@astrojs/tailwind";
-
 import { defineConfig, squooshImageService } from "astro/config";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), sitemap()],
   //image: {
   //  service: squooshImageService()
   //},
   output: "server",
-  adapter: netlify(),
+  adapter: netlify()
 });
