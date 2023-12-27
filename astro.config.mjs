21fe104cad4742b0504325cece4e4f04c79785d3
backas36 <backas36@gmail.com>
2023-12-27 22:48:21 +0800

    fix: build error, Could not transform image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 
-import mdx from "@astrojs/mdx";
+import { defineConfig, passthroughImageService } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx()],
+  image: {
+    service: passthroughImageService(),
+  },
 });
